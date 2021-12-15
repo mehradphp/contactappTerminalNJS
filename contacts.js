8e5af237facebb10e1d204ccc6be205d7e295c45
mehrdadphp <alikhodaverdi682@gmail.com>
2021-12-15 17:59:16 +0330

    saveJsonFile

--- contacts.js
+++ contacts.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
-// import fs from 'fs';
 // const chalk = require("chalk");
 const addcontacts = (fullname, phone, email) => {
     const contacts = loadContacts();
     const duplicateContact = contacts.find(c => c.fullname === fullname)
     if (!duplicateContact) {
         contacts.push({ fullname, phone, email });
+        saveContacs(contacts);
         console.log("conntact save");
     } else {
         console.log("contact alredy exist");
@@ -24,6 +24,10 @@ const loadContacts = () => {
         return [];
     }
 
+}
+const saveContacs = contacts => {
+    const data = JSON.stringify(contacts);
+    fs.writeFileSync("contacts.json", data)
 }
 
 module.exports = {
